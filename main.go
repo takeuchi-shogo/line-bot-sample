ac0e56f951c4ddb621c66e3aa6948fd57018104d
ShogoTakeuchi <smth.1.23@icloud.com>
2022-10-24 16:59:44 +0900

    commit

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"log"
+
 	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/takeuchi-shogo/line-bot-sample/config"
 )
 
 func main() {
-	config := NewConfig()
-	bot, err := linebot.New()
+
+	c := config.NewConfig()
+
+	bot, err := linebot.New(
+		c.ChannelSecret,
+		c.ChannelToken,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := linebot.NewTextMessage("Hello world")
+
+	if _, err = bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
 }
